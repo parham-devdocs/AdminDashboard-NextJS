0fb65d471ee6bf98461cc2c9e577c59e857efee1
parham-devdocs <p.77.pazargadi@gmail.com>
2024-09-26 17:11:12 +0330

    login page created

--- my-project/app/login/page.jsx
+++ my-project/app/login/page.jsx
@@ -1,5 +1,13 @@
+import classes from "./loginPage.module.css";
 export default function LoginPage() {
     return (
-        <h1>Login page</h1>
+        <div className={classes.container}>
+            <form className={classes.form}>
+                <h1 className={classes.header}>Login</h1>
+                <input type="text" placeholder="username" className={classes.input} />
+                <input type="text" placeholder="password" className={classes.input} />
+                <button type="submit" className={classes.btn}>Login</button>
+            </form>
+       </div>
     )
 }
